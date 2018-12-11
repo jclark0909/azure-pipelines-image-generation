4cc4ec012cd4c51d6bfdd6a70b0277e6822a1aad
jclark0909 <jclark0909@gmail.com>
2018-12-11 13:35:00 -0500

    Install Nuget package
    I am working on a Packer on-premise build and ran into an issue with running the script above. It will not install PSWindowsUpdate until Nuget package is installed. I recommend running the following piece of code prior to installing PSWindowsUpdate module.
    Install-PackageProvider -Name Nuget -Force

--- images/win/scripts/Installers/Install-WindowsUpdates.ps1
+++ images/win/scripts/Installers/Install-WindowsUpdates.ps1
@@ -6,6 +6,9 @@
 ################################################################################
 
 Write-Host "Run windows updates"
+#Need to install Negut prior to downloading PSWindowsUpdate module.
+Install-PackageProvider -Name Nuget -Force
+
 Install-Module -Name PSWindowsUpdate -Force -AllowClobber
 Get-WUInstall -WindowsUpdate -AcceptAll -UpdateType Software -IgnoreReboot
 Get-WUInstall -MicrosoftUpdate -AcceptAll -IgnoreUserInput -IgnoreReboot
